e6bd087226d0daaeb217d88243695516ce061861
notidman <laiker35@yandex.ru>
2022-11-06 04:29:38 +0400

    [upd]

--- init.el
+++ init.el
@@ -30,16 +30,3 @@
 ;; ElectricPair
 ;; puni
 ;; 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(yasnippet lsp-ivy lsp-ui use-package sly rainbow-mode rainbow-delimiters macrostep lsp-mode ivy evil company cherry-blossom-theme)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
