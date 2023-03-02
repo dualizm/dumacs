bda95778cfc20ef9b304a51ef5da85c54e74ea2c
cloezure <ognieff@yandex.ru>
2023-03-02 23:22:57 +0400

    [upd]

--- init.el
+++ init.el
@@ -1,14 +1,17 @@
-;;;;|
-;;;;| @AUTHORS cloezure <ognieff@yandex.ru>
-;;;;|
-;;;;| @URL https://github.com/cloezure/ezmacs
-;;;;|
-;;;;| @PROJECT-NAME ezmacs
-;;;;|
-;;;;| @DATE-START 29-09-22
-;;;;|
-;;;;| @BRIEF contains loadable modules for config.
-;;;;|
+(defvar ezmacs-info
+  '((file         . "init.el")
+    (authors      . "cloezure")
+    (email        . "ognieff@yandex.ru")
+    (url          . "https://github.com/cloezure/ezmacs")
+    (project-name . "ezmacs")
+    (date-start   . "29-09-22")
+    (brief        . "config for author")))
+
+(defun ezmacs-about (key)
+  (message (cdr (assoc key ezmacs-info))))
+
+;;; daemon
+(server-start)
 
 ;;; settings
 ;;; gui
@@ -17,16 +20,26 @@
 (if (fboundp 'menu-bar-mode)   (menu-bar-mode -1))
 
 ;;; font
-(set-frame-font
- "-PfEd-GohuFont NFM-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1"
- nil t)
+;;(set-frame-font
+;; "-PfEd-GohuFont NFM-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1"
+;; nil t)
 (set-face-attribute 'default nil :height 140)
 
+(defmacro unless-cond (&rest cases)
+  `(progn
+     ,@(mapcar #'(lambda (case) `(unless ,(car case) ,(cadr case))) cases)))
+
 ;;; auto-save
-(setq backup-directory-alist
-      `((".*" . "~/.emacs.d/.backups/")))
-(setq auto-save-file-name-transforms
-      `((".*" "~/.emacs.d/.auto-saves/" t)))
+(defun save-directories (backups saves)
+  (unless-cond
+   ((file-directory-p backups) (make-directory backups))
+   ((file-directory-p saves) (make-directory saves)))
+  (setq backup-directory-alist
+	`((".*" . ,backups)))
+  (setq auto-save-file-name-transforms
+	`((".*" ,saves t))))
+
+(save-directories "~/.emacs.d/.backups/" "~/.emacs.d/.auto-saves/")
 ;;; start screen
 (setq inhibit-startup-screen t)
 
@@ -57,8 +70,8 @@
 (package-initialize)
 
 (dolist (package '(use-package))
-   (unless (package-installed-p package)
-       (package-install package)))
+  (unless (package-installed-p package)
+    (package-install package)))
 
 (eval-and-compile
   (setq use-package-always-ensure t
@@ -66,6 +79,25 @@
 
 (eval-and-compile
   (require 'use-package))
+
+;;; auto-complete
+(use-package company
+  :init (global-company-mode t)
+  (setq company-idle-delay 0))
+
+;;; syntax checking
+(use-package flycheck
+  :init
+  (global-flycheck-mode t))
+
+;;; music player
+(use-package bongo)
+
+;;; highlight-indent-guides
+(use-package highlight-indent-guides
+  :config
+  (setq highlight-indent-guides-method 'bitmap)
+  :hook (prog-mode . highlight-indent-guides-mode))
 
 ;;; orgmode
 (defun org-mode-setup ()
@@ -81,27 +113,23 @@
   :hook
   (org-mode . org-bullets-mode))
 
-;;; global
-(use-package company
-  :init (global-company-mode t)
-  (setq company-idle-delay 0))
-
-;;; music player
-(use-package bongo)
-
+;;; pdf viewer
 (use-package pdf-tools)
-(use-package meson-mode)
+
 (use-package transpose-frame)
+
+;;; git client
 (use-package magit)
+
 (use-package vertico
   :init (vertico-mode))
-;;;(use-package vterm)
+
 (use-package avy
   :bind (("C-:" . 'avy-goto-char)
 	 ("C-'" . 'avy-goto-char-2)
 	 ("M-g l" . 'avy-goto-line)
 	 ("M-g f" . 'avy-goto-word-1)))
-  
+
 (use-package yasnippet
   :config (yas-global-mode t))
 
@@ -111,7 +139,6 @@
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
-(use-package makefile-executor)
 (use-package lsp-mode
   :hook ((c-mode . lsp-deferred)
 	 (c++-mode . lsp-deferred)))
@@ -125,36 +152,57 @@
 (use-package geiser-chicken)
 (use-package geiser-guile)
 
+;;; c/cxx
+(use-package meson-mode)
+(use-package makefile-executor)
+
+;;; nix
+(use-package nix-mode
+  :mode "\\.nix\\'")
+
 ;;; theme
 (use-package cherry-blossom-theme)
 (use-package naysayer-theme)
 (use-package orangey-bits-theme)
 (use-package jazz-theme)
-(load-theme 'jazz t)
+(load-theme 'cherry-blossom t)
 
 (defun toggle-window-split ()
   (interactive)
   (if (= (count-windows) 2)
       (let* ((this-win-buffer (window-buffer))
-         (next-win-buffer (window-buffer (next-window)))
-         (this-win-edges (window-edges (selected-window)))
-         (next-win-edges (window-edges (next-window)))
-         (this-win-2nd (not (and (<= (car this-win-edges)
-                     (car next-win-edges))
-                     (<= (cadr this-win-edges)
-                     (cadr next-win-edges)))))
-         (splitter
-          (if (= (car this-win-edges)
-             (car (window-edges (next-window))))
-          'split-window-horizontally
-        'split-window-vertically)))
-    (delete-other-windows)
-    (let ((first-win (selected-window)))
-      (funcall splitter)
-      (if this-win-2nd (other-window 1))
-      (set-window-buffer (selected-window) this-win-buffer)
-      (set-window-buffer (next-window) next-win-buffer)
-      (select-window first-win)
-      (if this-win-2nd (other-window 1))))))
+	     (next-win-buffer (window-buffer (next-window)))
+	     (this-win-edges (window-edges (selected-window)))
+	     (next-win-edges (window-edges (next-window)))
+	     (this-win-2nd (not (and (<= (car this-win-edges)
+					 (car next-win-edges))
+				     (<= (cadr this-win-edges)
+					 (cadr next-win-edges)))))
+	     (splitter
+	      (if (= (car this-win-edges)
+		     (car (window-edges (next-window))))
+		  'split-window-horizontally
+		'split-window-vertically)))
+	(delete-other-windows)
+	(let ((first-win (selected-window)))
+	  (funcall splitter)
+	  (if this-win-2nd (other-window 1))
+	  (set-window-buffer (selected-window) this-win-buffer)
+	  (set-window-buffer (next-window) next-win-buffer)
+	  (select-window first-win)
+	  (if this-win-2nd (other-window 1))))))
 (global-set-key (kbd "C-x |") 'toggle-window-split)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(flycheck highlight-indent-guides nix-mode yasnippet vertico use-package transpose-frame sly rainbow-mode rainbow-delimiters pdf-tools org-bullets orangey-bits-theme naysayer-theme meson-mode makefile-executor magit lsp-ui jazz-theme geiser-racket geiser-guile geiser-chicken company cherry-blossom-theme bongo avy)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
